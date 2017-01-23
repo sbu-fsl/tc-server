939e83118933e3ddd886203e89b395f0a076a7f4
Ming Chen <v.mingchen@gmail.com>
2017-01-23 11:24:20 -0500

    use UINT64_MAX to indicate copying whole file
    Signed-off-by: Ming Chen <v.mingchen@gmail.com>

--- src/cache_inode/cache_inode_copy.c
+++ src/cache_inode/cache_inode_copy.c
@@ -92,7 +92,7 @@ cache_inode_status_t cache_inode_copy(cache_entry_t *src_entry,
 		goto out;
 	}
 
-	if (count == 0) {
+	if (count == UINT64_MAX) {
 		count = src_entry->obj_handle->attrs->filesize - src_offset;
 		LogDebug(COMPONENT_CACHE_INODE,
 			 "0-count has an effective value of %zu", count);
